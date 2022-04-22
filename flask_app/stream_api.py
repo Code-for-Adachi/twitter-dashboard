e420e01f94b0a35ef7f64f74cf611a61743ba87f
yokobot <takuou@gmail.com>
2022-04-22 18:28:16 +0900

    苦しんでいるところをpush

--- flask_app/stream_api.py
+++ flask_app/stream_api.py
@@ -5,12 +5,20 @@ import tweepy
 from datetime import timedelta
 
 load_dotenv(override=True)
+api = tweepy.Client(os.getenv('TWITTER_API_BEARER_TOKEN'))
 
 class TweetPrinterV2(tweepy.StreamingClient):
-
     def on_tweet(self, tweet):
-        print(f"{tweet.id} {tweet.created_at} ({tweet.author_id}): {tweet.text}")
+        #print(f"{tweet.id} {tweet.created_at} ({tweet.author_id}): {tweet.text}")
+        print(api.get_tweet(id=tweet.id, expansions=["author_id"], user_fields=["username"]))
+        tw = api.get_tweet(id=tweet.id, expansions=["author_id"], user_fields=["username"])
+        url = "https://twitter.com/" + tw.includes["users"][0].username + "/status/" + str(tweet.id)
+        print(url)
         print("-"*50)
+
+    #def on_data(self, raw_data):
+    #    print(raw_data)
+    #    print("-"*50)
 
 def get_stream(word):
     printer = TweetPrinterV2(os.getenv('TWITTER_API_BEARER_TOKEN'))
@@ -19,3 +27,5 @@ def get_stream(word):
     rule = tweepy.StreamRule(value=word)
     printer.add_rules(rule)
     printer.filter()
+
+get_stream('足立区')
