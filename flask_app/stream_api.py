b4ae8215ba9c26285129272f83525356544839f7
yokobot <takuou@gmail.com>
2022-05-13 17:45:58 +0900

    refreshボタンの位置替えとusea nameのリンク直し

--- flask_app/stream_api.py
+++ flask_app/stream_api.py
@@ -14,29 +14,25 @@ client = tweepy.Client(bearer_token=os.getenv('TWITTER_API_BEARER_TOKEN'))
 
 class TweetPrinterV2(tweepy.StreamingClient):
     def on_tweet(self, status):
+        tw = client.get_tweet(id=int(status.id), expansions=["author_id"], user_fields=['id', 'name', 'username', 'profile_image_url'])
+        blob = TextBlob(status.text)
+        sent = blob.sentiment
+        polarity = sent.polarity
+        subjectivity = sent.subjectivity
 
-        user_field = client.get_tweet(id=status.id, user_fields=['id', 'name', 'username', 'profile_image_url'])
-        print(user_field.data)
+        tweet_item = {
+            'id_str': tw.includes["users"][0].id,
+            'text': status.text,
+            'polarity': polarity,
+            'subjectivity': subjectivity,
+            'username': tw.includes["users"][0].username,
+            'name': tw.includes["users"][0].name,
+            'profile_image_url': tw.includes["users"][0].profile_image_url,
+            'received_at': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
 
-        if ('RT @' not in status.text):
-            blob = TextBlob(status.text)
-            sent = blob.sentiment
-            polarity = sent.polarity
-            subjectivity = sent.subjectivity
-
-            tweet_item = {
-                'id_str': user_field.data['id'],
-                'text': status.text,
-                'polarity': polarity,
-                'subjectivity': subjectivity,
-                'username': user_field.data['username'],
-                'name': user_field.data['name'],
-                'profile_image_url': user_field.data['profile_image_url'],
-                'received_at': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            }
-
-            store.push(tweet_item)
-            print("Pushed to redis:", tweet_item)
+        store.push(tweet_item)
+        print("Pushed to redis:", tweet_item)
 
     def on_error(self, status_code):
         if status_code == 420:
